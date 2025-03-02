18891bab2420e2f3d68f51c34293fc518defab41
kojeffi <omondijeff88@gmail.com>
2025-03-02 22:46:59 +0300

    updates

--- accounts/models.py
+++ accounts/models.py
@@ -32,7 +32,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
