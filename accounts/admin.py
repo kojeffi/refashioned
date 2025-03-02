c40a57d6b79b338bfa428e8d6d628141bb66c89b
kojeffi <omondijeff88@gmail.com>
2025-03-02 22:08:52 +0300

    user login with email

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,31 +2,36 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Profile, Cart, CartItem, Order, OrderItem
 
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ("email", "first_name", "last_name", "is_staff", "is_active", "is_superuser")
-    list_filter = ("is_staff", "is_active", "is_superuser")
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
     
     fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        ("Personal Info", {"fields": ("first_name", "last_name")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
-        ("Important Dates", {"fields": ("last_login",)}),
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
     )
-    
+
     add_fieldsets = (
         (None, {
-            "classes": ("wide",),
-            "fields": ("email", "password1", "password2", "is_staff", "is_active", "is_superuser"),
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_superuser'),
         }),
     )
-    
-    search_fields = ("email",)
-    ordering = ("email",)
 
-# Register CustomUser in Django Admin
+    # Ensure Django uses email for authentication
+    def get_fieldsets(self, request, obj=None):
+        if not obj:
+            return self.add_fieldsets
+        return super().get_fieldsets(request, obj)
+
 admin.site.register(CustomUser, CustomUserAdmin)
-
 
 
 # ✅ Register other models
