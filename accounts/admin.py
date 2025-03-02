dca9a15ec8efa1e17fabf31c3c5e62c4f50bd757
kojeffi <omondijeff88@gmail.com>
2025-03-02 21:10:40 +0300

    user login with email

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import BaseUserAdmin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Profile, Cart, CartItem, Order, OrderItem
 
-class CustomUserAdmin(BaseUserAdmin):
+class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ("email", "first_name", "last_name", "is_staff", "is_active", "is_superuser")
     list_filter = ("is_staff", "is_active", "is_superuser")
